a61b0818061474dfa79205e94f12d63416d6e943
HerionZhou <996632222@qq.com>
2021-07-24 16:35:53 +0800

    7.24

---
+++ src/SwordToOffer/LeetCode698.java
@@ -0,0 +1,41 @@
+package SwordToOffer;
+
+public class LeetCode698 {
+    static class Solution {
+        boolean flag;
+        public boolean canPartitionKSubsets(int[] nums, int k) {
+            int sum = 0;
+            for (int num :nums){
+                sum += num;
+            }
+            int avg = sum / k;
+            if (avg * k != sum) return false;
+            int[] resSet = new int[k];
+            recur(nums, resSet, 0, avg);
+            return flag;
+        }
+
+        public void recur(int[] nums, int[]resSet, int i, int avg){
+            if (i == nums.length) {
+                for (int num : resSet){
+                    if (num != avg) return;
+                }
+                flag = true;
+                return;
+            }
+            for (int j = 0; j < resSet.length; j++){
+                if (resSet[j] + nums[i] <= avg){
+                    resSet[j] = resSet[j] + nums[i];
+                    recur(nums, resSet, i + 1, avg);
+                    resSet[j] = resSet[j] - nums[i];
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int[] test = {18,20,39,73,96,99,101,111,114,190,207,295,471,649,700,1037};
+        System.out.println(solution.canPartitionKSubsets(test, 4));
+    }
+}
--- src/SwordToOffer/SwordToOffer37.java
+++ src/SwordToOffer/SwordToOffer37.java
@@ -1,6 +1,5 @@
 package SwordToOffer;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
