c843f5383b8715e8411eed5e95a39e532a826852
HerionZhou <996632222@qq.com>
2021-09-16 21:20:51 +0800

    9.16

---
+++ src/Algorithm/SwordToOffer/LRUCache.java
@@ -0,0 +1,28 @@
+package Algorithm.SwordToOffer;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache extends LinkedHashMap<Integer, Integer> {
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        // accessOrder:get之后是否将这个节点放在双向链表尾部
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry eldest) {
+        return size() > capacity;
+    }
+
+}
